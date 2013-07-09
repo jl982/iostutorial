39557bd064dfe631110c528f1d37e3295515ced3
Jianneng <jl982@cornell.edu>
2013-07-09 01:13:23 -0400

    adding numbercard class files

---
+++ iOSTutorial/Model/NumberCard.h
@@ -0,0 +1,13 @@
+//
+//  NumberCard.h
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/9/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NumberCard : NSObject
+
+@end
---
+++ iOSTutorial/Model/NumberCard.m
@@ -0,0 +1,13 @@
+//
+//  NumberCard.m
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/9/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import "NumberCard.h"
+
+@implementation NumberCard
+
+@end
