e9c9350dc67d275d17e6f0260badb4ca2f43d746
Jianneng <jl982@cornell.edu>
2013-07-08 22:41:49 -0400

    Initial Commit

---
+++ iOSTutorial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/8/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iOSTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/8/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
