02f7c63379fb2eaf67dd3320e233be2171f5177e
Jianneng <jl982@cornell.edu>
2013-07-09 02:25:43 -0400

    flip card with numbers and flip count

--- iOSTutorial/ViewController.m
+++ iOSTutorial/ViewController.m
@@ -7,12 +7,43 @@
 //
 
 #import "ViewController.h"
+#import "NumberCardDeck.h"
 
 @interface ViewController ()
+@property (nonatomic) int flipCount;
+@property (strong, nonatomic) NumberCardDeck *deck;
 
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @end
 
+
 @implementation ViewController
+
+- (NumberCardDeck *)deck
+{
+    if (!_deck)
+    {
+        _deck = [[NumberCardDeck alloc] init];
+    }
+    return _deck;
+}
+
+- (IBAction)flipCard:(UIButton *)sender {
+    sender.selected = !sender.isSelected;
+    if (sender.isSelected)
+    {
+        Card *cardDrawn = [[self deck] drawRandomCard];
+        NSString *selectedText = cardDrawn ? cardDrawn.contents : @"Empty";
+        [sender setTitle:selectedText forState:UIControlStateSelected];
+    }
+    self.flipCount++;
+}
+
+- (void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
 
 - (void)viewDidLoad
 {
@@ -26,4 +57,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidUnload {
+    [self setFlipsLabel:nil];
+    [super viewDidUnload];
+}
 @end
