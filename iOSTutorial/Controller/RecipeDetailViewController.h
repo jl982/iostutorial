fcffab768c9a81cc9019fb7d253cb74ec239d70a
Jianneng <jl982@cornell.edu>
2013-07-10 01:45:18 -0400

    static recipe detail page (note addition of navigation controller

---
+++ iOSTutorial/Controller/RecipeDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeDetailViewController.h
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/10/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeDetailViewController : UIViewController
+
+@end
---
+++ iOSTutorial/Controller/RecipeDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  RecipeDetailViewController.m
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/10/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import "RecipeDetailViewController.h"
+
+@interface RecipeDetailViewController ()
+
+@end
+
+@implementation RecipeDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
