d50dde6c7c0cdf79064bb6c482a66b8c634367b0
Jianneng <jl982@cornell.edu>
2013-07-10 01:37:30 -0400

    added simple table view for recipe

---
+++ iOSTutorial/Controller/RecipeViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeViewController.h
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/10/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ iOSTutorial/Controller/RecipeViewController.m
@@ -0,0 +1,62 @@
+//
+//  RecipeViewController.m
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/10/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import "RecipeViewController.h"
+
+@interface RecipeViewController ()
+
+@end
+
+@implementation RecipeViewController {
+    NSArray *recipes;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    // Initialize table data
+    recipes = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"RecipeCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
