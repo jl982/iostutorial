df59ac922edfc153920dcc94a53a53d580a0ca48
Jianneng <jl982@cornell.edu>
2013-07-28 22:46:24 -0400

    added search view controller, which sends a GET request to localhost:9200/twitter/_search  when view is loaded

---
+++ iOSTutorial/Controller/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/28/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
---
+++ iOSTutorial/Controller/SearchViewController.m
@@ -0,0 +1,58 @@
+//
+//  SearchViewController.m
+//  iOSTutorial
+//
+//  Created by Jianneng on 7/28/13.
+//  Copyright (c) 2013 Jianneng Li. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setHTTPMethod:@"GET"];
+    [request setURL:[NSURL URLWithString:@"http://localhost:9200/twitter/_search"]];
+    
+    NSError *error = [[NSError alloc] init];
+    NSHTTPURLResponse *responseCode = nil;
+    
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
+    
+    NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+    NSLog(@"Server response:\n%@",returnString);
+    if (!returnData) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Request Error"
+                                                        message:@"Please try again later."
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
